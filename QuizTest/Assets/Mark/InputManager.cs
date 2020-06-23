d96608e12f01ba9b1075acbe88060062cf0a17bd
ItsM3rk <mswaantje@gmail.com>
2020-06-23 15:21:02 +0200

    Image request

---
+++ QuizTest/Assets/Mark/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject inputfield;
+    [SerializeField]
+    GameObject sprite;
+    string inputfieldtext;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U)) //set as u to currently test.
+        {
+            inputfieldtext = inputfield.gameObject.transform.GetChild(2).GetComponent<Text>().text;
+            Debug.Log(inputfieldtext);
+            Sprite sp = Resources.Load("QuizImages/quiz1/" + inputfieldtext + ".png") as Sprite;
+            sprite.GetComponent<SpriteRenderer>().sprite = sp;
+        }
+    }
+}
